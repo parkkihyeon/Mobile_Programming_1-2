b15ff94ba3afbb0f4257125dd32c845366b30f38
parkkihyeon <peaia@naver.com>
2015-11-21 23:20:25 +0900

    01_02 practice

---
+++ app/src/main/java/pre/pkh/mobileprogramming01_02/LoginActivity.java
@@ -0,0 +1,29 @@
+package pre.pkh.mobileprogramming01_02;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by inkyu on 15. 11. 5..
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.login);
+    }
+    public void onNewBtnBack(View v) {
+        Log.d("TEST", "BACK!!!");
+        finish();
+    }
+    public void onLoging(View v) {
+        EditText id = (EditText)findViewById(R.id.editTextID);
+        Toast.makeText(getApplicationContext(), id.getText(), Toast.LENGTH_LONG).show();
+    }
+
+}
---
+++ app/src/main/java/pre/pkh/mobileprogramming01_02/MainActivity.java
@@ -0,0 +1,51 @@
+package pre.pkh.mobileprogramming01_02;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("TEST", "oncreate1");
+        setContentView(R.layout.activity_main);
+        Button bt = (Button)this.findViewById(R.id.btnNew);
+        Button bt2 = (Button)this.findViewById(R.id.btnNew2) ;
+        Button bt3 = (Button)this.findViewById(R.id.btnNew3) ;
+
+        bt.setOnClickListener(new View.OnClickListener(){
+                                   public void onClick(View v) {
+                                       Toast.makeText(MainActivity.this.getApplicationContext(), "Click Button1", Toast.LENGTH_LONG).show();
+                                   }
+                              }
+        );
+
+        bt2.setOnClickListener(btnlistener2); //2
+
+            //3 리스너 구현
+            ButtonClickListener btnlistener = new ButtonClickListener();
+                bt3.setOnClickListener(btnlistener);
+    }
+    //2 익명 클래스
+    View.OnClickListener btnlistener2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Click Button2", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Click Button3", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
+
+
